19278facee32e4e775102b3ed3ab27b3d3281134
kotokz <kotokz@live.com>
2022-07-30 00:02:18 +0800

    fix redundant code and improve performance

--- src/main.rs
+++ src/main.rs
@@ -49,13 +49,13 @@ enum Pet {
     Horse,
 }
 
-#[derive(Debug, Default, Clone, Copy)]
+#[derive(Debug, Clone, Copy)]
 struct Entity {
-    house: Option<House>,
-    nation: Option<Nation>,
-    drink: Option<Drink>,
-    cigar: Option<Cigar>,
-    pet: Option<Pet>,
+    house: House,
+    nation: Nation,
+    drink: Drink,
+    cigar: Cigar,
+    pet: Pet,
 }
 
 impl Cigar {
@@ -156,22 +156,19 @@ enum Rule {
     Rule15,
 }
 
-struct Contrains {
+struct Constraint {
     results: HashMap<Rule, bool>,
 }
 
-impl Contrains {
+impl Constraint {
     fn default() -> Self {
-        let mut hashmap: HashMap<Rule, bool> = HashMap::new();
-        hashmap.insert(Rule::Rule8, true);
-        hashmap.insert(Rule::Rule9, true);
-        Contrains { results: hashmap }
+        Constraint {
+            results: HashMap::new(),
+        }
     }
 
     fn check(&self) -> bool {
-        let result = self.results.iter().map(|(_, v)| v).all(|v| *v);
-
-        result
+        self.results.iter().map(|(_, v)| v).all(|v| *v)
     }
 
     fn all_passed(&self) -> bool {
@@ -179,79 +176,86 @@ impl Contrains {
         count == 15
     }
     fn satisfies_ent(&mut self, entities: &[Entity]) -> Option<bool> {
+        if let Some(e) = entities.get(0) {
+            if e.nation == Nation::Norwegian {
+                self.results.insert(Rule::Rule9, true);
+            } else {
+                self.results.insert(Rule::Rule9, false);
+            }
+        }
+
+        if let Some(e) = entities.get(2) {
+            if e.drink == Drink::Milk {
+                self.results.insert(Rule::Rule8, true);
+            } else {
+                self.results.insert(Rule::Rule8, false);
+            }
+        }
         for item in entities.iter() {
-            if let Some(nation) = item.nation {
-                match nation {
-                    Nation::British => {
-                        if Some(House::Red) == item.house {
-                            self.results.insert(Rule::Rule1, true);
-                        } else {
-                            self.results.insert(Rule::Rule1, false);
-                        }
+            match item.nation {
+                Nation::British => {
+                    if House::Red == item.house {
+                        self.results.insert(Rule::Rule1, true);
+                    } else {
+                        self.results.insert(Rule::Rule1, false);
                     }
-                    Nation::Swedish => {
-                        if Some(Pet::Dog) == item.pet {
-                            self.results.insert(Rule::Rule2, true);
-                        } else {
-                            self.results.insert(Rule::Rule2, false);
-                        }
-                    }
-                    Nation::Danish => {
-                        if Some(Drink::Tea) == item.drink {
-                            self.results.insert(Rule::Rule3, true);
-                        } else {
-                            self.results.insert(Rule::Rule3, false);
-                        }
-                    }
-                    _ => {}
                 }
+                Nation::Swedish => {
+                    if Pet::Dog == item.pet {
+                        self.results.insert(Rule::Rule2, true);
+                    } else {
+                        self.results.insert(Rule::Rule2, false);
+                    }
+                }
+                Nation::Danish => {
+                    if Drink::Tea == item.drink {
+                        self.results.insert(Rule::Rule3, true);
+                    } else {
+                        self.results.insert(Rule::Rule3, false);
+                    }
+                }
+                _ => {}
             }
-
-            if let Some(house) = item.house {
-                match house {
-                    House::Green => {
-                        if Some(Drink::Coffee) == item.drink {
-                            self.results.insert(Rule::Rule5, true);
-                        } else {
-                            self.results.insert(Rule::Rule5, false);
-                        }
+            match item.house {
+                House::Green => {
+                    if Drink::Coffee == item.drink {
+                        self.results.insert(Rule::Rule5, true);
+                    } else {
+                        self.results.insert(Rule::Rule5, false);
                     }
-                    House::Yellow => {
-                        if Some(Cigar::Dunhill) == item.cigar {
-                            self.results.insert(Rule::Rule7, true);
-                        } else {
-                            self.results.insert(Rule::Rule7, false);
-                        }
-                    }
-                    _ => {}
                 }
+                House::Yellow => {
+                    if Cigar::Dunhill == item.cigar {
+                        self.results.insert(Rule::Rule7, true);
+                    } else {
+                        self.results.insert(Rule::Rule7, false);
+                    }
+                }
+                _ => {}
             }
-
-            if let Some(cigar) = item.cigar {
-                match cigar {
-                    Cigar::Pall => {
-                        if Some(Pet::Bird) == item.pet {
-                            self.results.insert(Rule::Rule6, true);
-                        } else {
-                            self.results.insert(Rule::Rule6, false);
-                        }
+            match item.cigar {
+                Cigar::Pall => {
+                    if Pet::Bird == item.pet {
+                        self.results.insert(Rule::Rule6, true);
+                    } else {
+                        self.results.insert(Rule::Rule6, false);
                     }
-                    Cigar::Master => {
-                        if Some(Drink::Beer) == item.drink {
-                            self.results.insert(Rule::Rule12, true);
-                        } else {
-                            self.results.insert(Rule::Rule12, false);
-                        }
-                    }
-                    Cigar::Prince => {
-                        if Some(Nation::German) == item.nation {
-                            self.results.insert(Rule::Rule13, true);
-                        } else {
-                            self.results.insert(Rule::Rule13, false);
-                        }
-                    }
-                    _ => {}
                 }
+                Cigar::Master => {
+                    if Drink::Beer == item.drink {
+                        self.results.insert(Rule::Rule12, true);
+                    } else {
+                        self.results.insert(Rule::Rule12, false);
+                    }
+                }
+                Cigar::Prince => {
+                    if Nation::German == item.nation {
+                        self.results.insert(Rule::Rule13, true);
+                    } else {
+                        self.results.insert(Rule::Rule13, false);
+                    }
+                }
+                _ => {}
             }
         }
         None
@@ -259,63 +263,43 @@ impl Contrains {
 
     fn satisfies_cmp(&mut self, entities: &[Entity]) -> Option<bool> {
         for pair in entities.windows(2) {
-            if Some(House::Green) == pair[0].house {
-                if let Some(rhouse) = pair[1].house {
-                    if rhouse == House::White {
-                        self.results.insert(Rule::Rule4, true);
-                    } else {
-                        self.results.insert(Rule::Rule4, false);
-                    }
+            if House::Green == pair[0].house {
+                if House::White == pair[1].house {
+                    self.results.insert(Rule::Rule4, true);
+                } else {
+                    self.results.insert(Rule::Rule4, false);
                 }
             }
 
-            if let Some(pet) = pair[0].pet {
-                match pet {
-                    Pet::Cat => {
-                        if let Some(r_cigar) = pair[1].cigar {
-                            if r_cigar == Cigar::Blend {
-                                self.results.insert(Rule::Rule10, true);
-                            } else {
-                                self.results.insert(Rule::Rule10, false);
-                            }
-                        }
-                    }
-                    Pet::Horse => {
-                        if Some(Cigar::Dunhill) == pair[1].cigar {
-                            self.results.insert(Rule::Rule11, true);
-                        }
-                    }
-                    _ => {}
+            if Pet::Cat == pair[0].pet {
+                if pair[1].cigar == Cigar::Blend {
+                    self.results.insert(Rule::Rule10, true);
+                } else {
+                    self.results.insert(Rule::Rule10, false);
                 }
             }
 
-            if let Some(cigar) = pair[0].cigar {
-                match cigar {
-                    Cigar::Dunhill => {
-                        if Some(Pet::Horse) == pair[1].pet {
-                            self.results.insert(Rule::Rule11, true);
-                        }
-                    }
-                    Cigar::Blend => {
-                        if Some(Drink::Water) == pair[1].drink {
-                            self.results.insert(Rule::Rule15, true);
-                        }
-                    }
-                    _ => {}
+            if Pet::Horse == pair[0].pet && Cigar::Dunhill == pair[1].cigar {
+                self.results.insert(Rule::Rule11, true);
+            }
+
+            if Cigar::Dunhill == pair[0].cigar && Pet::Horse == pair[1].pet {
+                self.results.insert(Rule::Rule11, true);
+            }
+
+            if Cigar::Blend == pair[0].cigar && Drink::Water == pair[1].drink {
+                self.results.insert(Rule::Rule15, true);
+            }
+
+            if Nation::Norwegian == pair[0].nation {
+                if House::Blue == pair[1].house {
+                    self.results.insert(Rule::Rule14, true);
+                } else {
+                    self.results.insert(Rule::Rule14, false);
                 }
             }
 
-            if Some(Nation::Norwegian) == pair[0].nation {
-                if let Some(r_house) = pair[1].house {
-                    if r_house == House::Blue {
-                        self.results.insert(Rule::Rule14, true);
-                    } else {
-                        self.results.insert(Rule::Rule14, false);
-                    }
-                }
-            }
-
-            if Some(Drink::Water) == pair[0].drink && Some(Cigar::Blend) == pair[1].cigar {
+            if Drink::Water == pair[0].drink && Cigar::Blend == pair[1].cigar {
                 self.results.insert(Rule::Rule15, true);
             }
         }
@@ -329,91 +313,68 @@ impl Contrains {
     }
 }
 
-fn assign_ent(entity: &Entity, entities: &[Entity]) -> Vec<Entity> {
+fn assign_ent(entities: &[Entity]) -> Vec<Entity> {
     let mut dis_cigar: HashSet<Cigar> = HashSet::new();
     let mut dis_drink: HashSet<Drink> = HashSet::new();
     let mut dis_house: HashSet<House> = HashSet::new();
     let mut dis_nation: HashSet<Nation> = HashSet::new();
     let mut dis_pet: HashSet<Pet> = HashSet::new();
     for item in entities.iter() {
-        dis_cigar.insert(item.cigar.unwrap());
-        dis_drink.insert(item.drink.unwrap());
-        dis_house.insert(item.house.unwrap());
-        dis_nation.insert(item.nation.unwrap());
-        dis_pet.insert(item.pet.unwrap());
+        dis_cigar.insert(item.cigar);
+        dis_drink.insert(item.drink);
+        dis_house.insert(item.house);
+        dis_nation.insert(item.nation);
+        dis_pet.insert(item.pet);
     }
-    let mut possibiles = vec![];
+    let mut possibilities = vec![];
     for cigar in Cigar::iter() {
-        let cigar = match entity.cigar {
-            Some(c) => c,
-            None => cigar,
-        };
         if dis_cigar.contains(&cigar) {
             continue;
         }
+
         for drink in Drink::iter() {
             if dis_drink.contains(&drink) {
                 continue;
             }
-            let drink = match entity.drink {
-                Some(d) => d,
-                None => drink,
-            };
             for house in House::iter() {
                 if dis_house.contains(&house) {
                     continue;
                 }
-                let house = match entity.house {
-                    Some(h) => h,
-                    None => house,
-                };
                 for nation in Nation::iter() {
                     if dis_nation.contains(&nation) {
                         continue;
                     }
-                    let nation = match entity.nation {
-                        Some(n) => n,
-                        None => nation,
-                    };
                     for pet in Pet::iter() {
                         if dis_pet.contains(&pet) {
                             continue;
                         }
-                        let pet = match entity.pet {
-                            Some(p) => p,
-                            None => pet,
-                        };
 
                         let local_ent = Entity {
-                            cigar: Some(cigar),
-                            drink: Some(drink),
-                            house: Some(house),
-                            nation: Some(nation),
-                            pet: Some(pet),
+                            cigar,
+                            drink,
+                            house,
+                            nation,
+                            pet,
                         };
 
-                        let mut constrain = Contrains::default();
+                        let mut constrain = Constraint::default();
                         let mut temp_ents = entities.to_vec();
                         temp_ents.push(local_ent);
 
                         if constrain.satisfies(&temp_ents) {
-                            possibiles.push(local_ent);
+                            possibilities.push(local_ent);
                         }
                     }
                 }
             }
         }
     }
-    possibiles
+    possibilities
 }
 
-fn backtracking_search(
-    ents: &[Entity],
-    confirmed: &Vec<Entity>,
-    candidates: &[Entity],
-) -> Option<Vec<Entity>> {
-    if ents.len() == confirmed.len() {
-        let mut constrain = Contrains::default();
+fn backtracking_search(length: usize, confirmed: &Vec<Entity>) -> Option<Vec<Entity>> {
+    if length == confirmed.len() {
+        let mut constrain = Constraint::default();
         if constrain.satisfies(confirmed) && constrain.all_passed() {
             return Some(confirmed.to_vec());
         } else {
@@ -421,55 +382,26 @@ fn backtracking_search(
         }
     }
 
-    let mut new_candidates = vec![];
-    let mut possibles = vec![];
-
-    for (index, item) in candidates.iter().enumerate() {
-        if index == 0 {
-            possibles = assign_ent(item, confirmed);
-        } else {
-            new_candidates.push(*item);
-        }
-    }
+    let possibles = assign_ent(confirmed);
 
     for ent in possibles.iter() {
         // find a possible entity candidate
         let mut next_confirmed = confirmed.clone();
         next_confirmed.push(*ent);
-        if let Some(result) = backtracking_search(ents, &next_confirmed, &new_candidates) {
+        if let Some(result) = backtracking_search(length, &next_confirmed) {
             return Some(result);
         }
     }
     None
 }
 
-fn search(ents: &[Entity]) -> Option<Vec<Entity>> {
-    let mut candidates = vec![];
-    for ent in ents.iter() {
-        candidates.push(*ent);
-    }
-    backtracking_search(ents, &vec![], &candidates)
-}
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut entities = [Entity::default(); 5];
-    if let Some(first) = entities.get_mut(0) {
-        first.nation = Some(Nation::Norwegian);
-    }
-    if let Some(middle) = entities.get_mut(2) {
-        middle.drink = Some(Drink::Milk);
-    }
+    let entities = backtracking_search(5, &vec![]).ok_or("Failed to solve the puzzle")?;
 
-    let entities = search(&entities).ok_or("Failed to solve the riddle")?;
-
-    for ent in entities.iter() {
+    for (index, ent) in entities.iter().enumerate() {
         println!(
-            "{:?}, {:?}, {:?}, {:?}, {:?}",
-            ent.house.unwrap(),
-            ent.nation.unwrap(),
-            ent.drink.unwrap(),
-            ent.cigar.unwrap(),
-            ent.pet.unwrap()
+            "{}: {:?}, {:?}, {:?}, {:?}, {:?}",
+            index, ent.house, ent.nation, ent.drink, ent.cigar, ent.pet
         );
     }
 
